e0dce40e3d4705b221c602c55375298966cb5db1
Thang NGUYEN MANH <tnmanh@linagora.com>
2020-06-08 15:31:05 +0700

    Use webpack build on production

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const AwesomeModule = require('awesome-module');
 const Dependency = AwesomeModule.AwesomeModuleDependency;
 const glob = require('glob-all');
 const FRONTEND_JS_PATH = __dirname + '/frontend/app/';
+const FRONTEND_JS_PATH_BUILD = __dirname + '/dist/';
 const AWESOME_MODULE_NAME = 'linagora.esn.dav.import';
 
 const awesomeModule = new AwesomeModule(AWESOME_MODULE_NAME, {
@@ -45,13 +46,22 @@ const awesomeModule = new AwesomeModule(AWESOME_MODULE_NAME, {
       const webserverWrapper = dependencies('webserver-wrapper');
 
       // Register every exposed frontend scripts
-      const frontendJsFilesFullPath = glob.sync([
-        FRONTEND_JS_PATH + '**/*.module.js',
-        FRONTEND_JS_PATH + '**/!(*spec).js'
-      ]);
-      const frontendJsFilesUri = frontendJsFilesFullPath.map(function(filepath) {
-        return filepath.replace(FRONTEND_JS_PATH, '');
-      });
+      let frontendJsFilesFullPath, frontendJsFilesUri;
+
+      if (process.env.NODE_ENV !== 'production') {
+        frontendJsFilesFullPath = glob.sync([
+          FRONTEND_JS_PATH + '**/*.module.js',
+          FRONTEND_JS_PATH + '**/!(*spec).js'
+        ]);
+
+        frontendJsFilesUri = frontendJsFilesFullPath.map(filepath => filepath.replace(FRONTEND_JS_PATH, ''));
+      } else {
+        frontendJsFilesFullPath = glob.sync([
+          FRONTEND_JS_PATH_BUILD + '*.js'
+        ]);
+
+        frontendJsFilesUri = frontendJsFilesFullPath.map(filepath => filepath.replace(FRONTEND_JS_PATH_BUILD, ''));
+      }
 
       webserverWrapper.injectAngularAppModules(AWESOME_MODULE_NAME, frontendJsFilesUri, AWESOME_MODULE_NAME, ['esn'], {
         localJsFiles: frontendJsFilesFullPath
